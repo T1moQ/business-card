9790eb507780809bd14174e11660a8294545d2a0
Timofey <pttima@yandex.ru>
2025-06-06 01:01:48 +0300

    feat: lots of stuff

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,40 +1,29 @@
 import type { FC } from 'react'
 import { motion } from 'framer-motion'
-import { Hero } from '../components/hero'
+import Lightning from '../motion/Backgrounds/Lightning/Lightning'
 
 export const MainPage: FC = () => {
 	return (
-		<main className="min-h-screen overflow-hidden bg-gray-100 relative flex items-center justify-center">
-			<motion.div
-				initial={{ opacity: 0, scale: 1.1 }}
-				animate={{ opacity: 1, scale: 1 }}
-				transition={{ duration: 1 }}
-				className="absolute bottom-0 left-0 w-full h-full z-0"
-				style={{
-					background:
-						'linear-gradient(180deg, rgba(124, 41, 85, 1) 0%, rgba(198, 88, 55, 1) 46%, rgba(213, 70, 54, 1) 53%, rgba(184, 28, 68, 1) 100%)',
-				}}
-			/>
-
-			{/* 2. Персонаж */}
+		<main className="min-h-screen overflow-hidden bg-gray-100 relative">
+			<div className="w-1/2 h-[950px] absolute top-0 right-0 z-0">
+				<Lightning hue={280} xOffset={0} speed={1} intensity={0.5} size={0.5} />
+			</div>
+			<div className="w-1/2 h-[950px] absolute top-0 left-0 rotate-180 z-0">
+				<Lightning hue={280} xOffset={0} speed={1} intensity={0.5} size={0.5} />
+			</div>
 			<motion.img
-				src="/public/char.png"
-				alt="Персонаж"
-				initial={{ opacity: 0, y: -100 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ delay: 2, duration: 2 }}
-				className="w-[440px] z-10 "
+				initial={{ opacity: 0, scale: 0.8 }}
+				animate={{ opacity: 1, scale: 1.1 }}
+				transition={{ duration: 3, delay: 1.5, ease: 'easeInOut' }}
+				src="./public/char.png"
+				className="absolute inset-x-0 m-auto top-8 w-[400px]"
 			/>
-
-			{/* 3. Афиша */}
 			<motion.div
-				initial={{ opacity: 0, scale: 0.8, y: 50 }}
-				animate={{ opacity: 1, scale: 1, y: 0 }}
-				transition={{ delay: 3, duration: 2.5 }}
-				className="absolute right-96 top-96 bg-white p-6 border-2 border-black shadow-xl z-20 max-w-sm "
-			>
-				<Hero />
-			</motion.div>
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 1.5, ease: 'easeInOut' }}
+				className="w-1/2 h-[950px] absolute top-0 right-0 z-0"
+			></motion.div>
 		</main>
 	)
 }
