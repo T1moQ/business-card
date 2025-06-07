30b1da88227833a123cc285b0554311d9c6344ed
Timofey <pttima@yandex.ru>
2025-06-07 14:58:50 +0300

    feat: name logos added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react'
 export const Hero: FC = () => {
 	return (
 		<div>
-			<h1 className="text-5xl">AZAZAZAZ</h1>
+			<h1>Hay everyone, I am Timofey Ptichenko</h1>
 		</div>
 	)
 }
--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,29 +1,80 @@
 import type { FC } from 'react'
 import { motion } from 'framer-motion'
-import Lightning from '../motion/Backgrounds/Lightning/Lightning'
+// import Lightning from '../motion/Backgrounds/Lightning/Lightning'
 
 export const MainPage: FC = () => {
 	return (
-		<main className="min-h-screen overflow-hidden bg-gray-100 relative">
-			<div className="w-1/2 h-[950px] absolute top-0 right-0 z-0">
-				<Lightning hue={280} xOffset={0} speed={1} intensity={0.5} size={0.5} />
-			</div>
-			<div className="w-1/2 h-[950px] absolute top-0 left-0 rotate-180 z-0">
-				<Lightning hue={280} xOffset={0} speed={1} intensity={0.5} size={0.5} />
-			</div>
+		<main className="min-h-screen overflow-hidden bg-black relative">
+			{/* <motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 0.5, ease: 'easeIn' }}
+			>
+				<div className="w-1/3 h-[950px] absolute top-0 right-0 z-0">
+					<Lightning
+						hue={280}
+						xOffset={-0.5}
+						speed={1}
+						intensity={1}
+						size={1}
+					/>
+				</div>
+				<div className="w-1/3 h-[950px] absolute top-0 left-0 rotate-180 z-0">
+					<Lightning
+						hue={280}
+						xOffset={-0.5}
+						speed={1}
+						intensity={1}
+						size={1}
+					/>
+				</div>
+			</motion.div> */}
 			<motion.img
 				initial={{ opacity: 0, scale: 0.8 }}
 				animate={{ opacity: 1, scale: 1.1 }}
 				transition={{ duration: 3, delay: 1.5, ease: 'easeInOut' }}
 				src="./public/char.png"
-				className="absolute inset-x-0 m-auto top-8 w-[400px]"
+				className="absolute inset-x-0 m-auto top-8 w-[400px] z-30"
 			/>
 			<motion.div
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
-				transition={{ duration: 3, delay: 1.5, ease: 'easeInOut' }}
-				className="w-1/2 h-[950px] absolute top-0 right-0 z-0"
-			></motion.div>
+				transition={{ duration: 3, delay: 3.5, ease: 'easeInOut' }}
+				className="absolute inset-x-0 m-auto top-[360px] w-[1100px] h-[560px] rounded-tl-4xl rounded-tr-4xl overflow-visible"
+			>
+				<img
+					src="./public/ripped.png"
+					className="absolute w-[1100px] rotate-180 -top-11"
+				/>
+				<img
+					src="./public/ripped.png"
+					className="absolute w-[570px] rotate-90 top-68 -left-72"
+				/>
+				<img
+					src="./public/ripped.png"
+					className="absolute w-[570px] rotate-270 top-72 -right-71"
+				/>
+			</motion.div>
+			<motion.img
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 3.5, ease: 'easeInOut' }}
+				src="./public/f-name-logo.png"
+				className="w-104 absolute top-10 left-104"
+			/>
+			<motion.img
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 3.5, ease: 'easeInOut' }}
+				src="./public/l-name-logo.png"
+				className="w-104 absolute top-10 right-106"
+			/>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 3, delay: 3.5, ease: 'easeInOut' }}
+				className="absolute bg-zinc-900 w-[1000px] h-124 rounded-tr-4xl rounded-tl-4xl bottom-0 left-1/2 -translate-x-1/2"
+			/>
 		</main>
 	)
 }
