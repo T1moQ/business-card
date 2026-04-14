82980b75ca35ef813f7dd18d3d19a33639e37fa2
TimoQ <pttima@yandex.ru>
2026-04-14 18:18:27 +0300

    fix: minor fixes

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -151,7 +151,7 @@ export const MainPage: FC = () => {
 							delay: 1,
 							ease: ['easeIn', 'linear', 'easeOut'],
 						}}
-						className="relative z-10 flex flex-col items-center sm:max-w-[540px] md:max-w-[600px] lg:max-w-[924px] lg:h-[724px] xl:max-w-[1200px] 3xl:max-w-[1400px] md:px-0 xl:-mt-24 md:-mt-14 lg:-mt-17 sm:-mt-12 3xl:-mt-28"
+						className="desktop-card-shell relative z-10 flex flex-col items-center sm:max-w-[540px] md:max-w-[600px] lg:max-w-[924px] lg:h-[724px] xl:max-w-[1200px] 3xl:max-w-[1400px] md:px-0 xl:-mt-24 md:-mt-14 lg:-mt-17 sm:-mt-12 3xl:-mt-28"
 					>
 						<div className="flex flex-col w-full">
 							<img src="/rippedBG1.png" className="w-full" />
@@ -159,7 +159,7 @@ export const MainPage: FC = () => {
 						</div>
 
 						{/* Main Card */}
-						<div className="absolute xl:top-24 lg:top-16 md:top-12 sm:top-12 sm:w-[75%] md:w-[85%] lg:w-[812px] xl:w-[1040px] 3xl:w-[1280px] pb-6 md:pb-8 xl:pb-10 bg-zinc-900 sm:rounded-tr-2xl sm:rounded-tl-2xl lg:rounded-tr-3xl lg:rounded-tl-3xl xl:rounded-tr-4xl xl:rounded-tl-4xl md:pt-5 md:px-4 lg:px-10 xl:px-8 flex justify-center lg:h-[650px] md:h-[80%] lg:pt-8 2xl:pt-5 3xl:pt-8">
+						<div className="desktop-main-card absolute xl:top-24 lg:top-16 md:top-12 sm:top-12 sm:w-[75%] md:w-[85%] lg:w-[812px] xl:w-[1040px] 3xl:w-[1280px] pb-6 md:pb-8 xl:pb-10 bg-zinc-900 sm:rounded-tr-2xl sm:rounded-tl-2xl lg:rounded-tr-3xl lg:rounded-tl-3xl xl:rounded-tr-4xl xl:rounded-tl-4xl md:pt-5 md:px-4 lg:px-10 xl:px-8 flex justify-center lg:h-[650px] md:h-[80%] lg:pt-8 2xl:pt-5 3xl:pt-8">
 							<div className="w-full h-full">
 								<Card />
 							</div>
@@ -171,14 +171,14 @@ export const MainPage: FC = () => {
 							animate={{ opacity: 1 }}
 							transition={{ duration: 3, delay: 8, ease: 'easeIn' }}
 							src="/skull1.png"
-							className="absolute z-30 sm:w-72 md:w-56 lg:w-60 xl:w-[280px] 3xl:w-[400px] sm:-left-36 sm:-bottom-32 md:-left-24 lg:-left-20 xl:-left-10 md:-rotate-20 xl:-rotate-25 md:bottom-8 lg:bottom-10 xl:bottom-18 2xl:bottom-32 3xl:-bottom-20 3xl:-left-36"
+							className="desktop-skull-left absolute z-30 sm:w-72 md:w-56 lg:w-60 xl:w-[280px] 3xl:w-[400px] sm:-left-36 sm:-bottom-32 md:-left-24 lg:-left-20 xl:-left-10 md:-rotate-20 xl:-rotate-25 md:bottom-8 lg:bottom-10 xl:bottom-18 2xl:bottom-32 3xl:-bottom-20 3xl:-left-36"
 						/>
 						<motion.img
 							initial={{ opacity: 0 }}
 							animate={{ opacity: 1 }}
 							transition={{ duration: 3, delay: 8, ease: 'easeIn' }}
 							src="/skull1.png"
-							className="absolute z-30 sm:w-72 md:w-52 lg:w-58 xl:w-[300px] 3xl:w-[420px] sm:-right-36 sm:-bottom-32 md:-right-16 lg:-right-12 xl:-right-10 md:bottom-7 lg:bottom-8 xl:bottom-17 2xl:bottom-32 3xl:-bottom-20 3xl:-right-30 scale-x-[-1] md:rotate-20 xl:rotate-25"
+							className="desktop-skull-right absolute z-30 sm:w-72 md:w-52 lg:w-58 xl:w-[300px] 3xl:w-[420px] sm:-right-36 sm:-bottom-32 md:-right-16 lg:-right-12 xl:-right-10 md:bottom-7 lg:bottom-8 xl:bottom-17 2xl:bottom-32 3xl:-bottom-20 3xl:-right-30 scale-x-[-1] md:rotate-20 xl:rotate-25"
 						/>
 					</motion.div>
 
